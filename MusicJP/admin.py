8059aed1fcaa104d5b5983c0124d76f7fbbe20c8
jorge <noemail@>
2017-03-22 19:11:04 +0100

    Backend to test models, and proper configuration with sqlite

---
+++ MusicJP/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from models import *
+
+
+# Register your models here.
+
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Playlist)
+admin.site.register(PlaylistTrack)
+admin.site.register(MediaType)
+admin.site.register(Track)
+admin.site.register(Genre)
+admin.site.register(Customer)
+admin.site.register(Employee)
+admin.site.register(Invoice)
+admin.site.register(Invoiceline)
+
+
+
+
