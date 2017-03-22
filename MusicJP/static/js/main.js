b3a6f1414d6ac83b62806c105fa38ab85ba7d1fb
jorge <noemail@>
2017-03-22 19:22:29 +0100

    Bootstrap libs

---
+++ MusicJP/static/js/main.js
@@ -0,0 +1,12 @@
+$( "h1 + ul > li" ).on( "click", function() {
+
+    $(this).next().slideToggle();
+    var span = $(this).find("span");
+    if ($(span).hasClass("glyphicon-plus")){
+        $(span).removeClass("glyphicon-plus");
+        $(span).addClass("glyphicon-menu-up");
+    }else {
+        $(span).removeClass("glyphicon-menu-up");
+        $(span).addClass("glyphicon-plus");
+    }
+});
